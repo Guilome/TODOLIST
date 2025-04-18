049a605933d0f8ca892c463b804401aa99aa3a6c
Guillaume <guillaume.gobert@gmail.com>
2025-04-18 11:11:52 +0200

    gestion de la page d'acceuil

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+const url_taches = location.origin + "/todos-front-main/tasks.html";
+const dialog = document.querySelector("dialog");
+const fermerModal = document.querySelector('button.close');
+const inputPrenom = document.getElementById('prenom');
+
+form.addEventListener('submit', (e) => {
+    try {
+        e.preventDefault();
+        verifierPrenom(inputPrenom.value);
+        localStorage.setItem('prenom_todo', JSON.stringify(prenom.value));
+        window.location = url_taches;
+    } catch (error) {
+        inputPrenom.classList.add('error');
+        console.error(error.message);
+        dialog.showModal();
+    }
+})
+
+fermerModal.addEventListener('click', function() {
+    dialog.close()
+})
+
+inputPrenom.addEventListener('focusin', (e) => {
+    inputPrenom.classList.remove('error');
+})
+
+function verifierPrenom(prenom) {
+    if (prenom === "" || prenom === null) {
+        throw new Error(`Le prénom est vide.`)
+    }
+}
