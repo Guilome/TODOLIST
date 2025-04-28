d6a9355d3dc593d2b78f7634fb33b2f7510e6333
Guillaume <guillaume.gobert@gmail.com>
2025-04-28 12:59:02 +0200

    ajustement test prenom

--- js/index.js
+++ js/index.js
@@ -1,33 +1,34 @@
 const form = document.querySelector('form');
 const dialog = document.querySelector("dialog");
 const fermerModal = document.querySelector('button.close');
-const inputPrenom = document.getElementById('prenom');
+const inputPrenom = document.getElementById('prenom')
 
 form.addEventListener('submit', (e) => {
     try {
         e.preventDefault();
-        verifierPrenom(inputPrenom.value);
-        localStorage.setItem('prenom_todo', JSON.stringify(prenom.value));
-        navigation('tasks.html')
+        submitPrenom();
     } catch (error) {
-        inputPrenom.classList.add('error');
         console.error(error.message);
-        setTitreModal('Erreur');
-        setTexteModal('Vous devez d\'abord renseigner un prénom avant d\'accéder à la liste des tâches.');
-        dialog.showModal();
     }
-})
+});
 
 fermerModal.addEventListener('click', function() {
     dialog.close()
-})
+});
 
 inputPrenom.addEventListener('focusin', (e) => {
     inputPrenom.classList.remove('error');
-})
+});
 
-function verifierPrenom(prenom) {
-    if (prenom === "" || prenom === null) {
+function submitPrenom() {
+    if (inputPrenom.value === "" || inputPrenom.value === null) {
+        inputPrenom.classList.add('error');
+        setTitreModal('Erreur');
+        setTexteModal('Vous devez d\'abord renseigner un prénom avant d\'accéder à la liste des tâches.');
+        dialog.showModal();
         throw new Error(`Le prénom est vide.`)
+    } else {
+        localStorage.setItem('prenom_todo', JSON.stringify(inputPrenom.value));
+        navigation('tasks.html');
     }
 }
